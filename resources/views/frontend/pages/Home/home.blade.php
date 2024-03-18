ee56dda779375ca5889aaa7fe28f387982b572a1
Md-Lutfar-Rahman <lutfarrahmaninfo@gmail.com>
2024-03-18 15:25:43 +0600

    Projects section Added

--- resources/views/frontend/pages/Home/home.blade.php
+++ resources/views/frontend/pages/Home/home.blade.php
@@ -6,5 +6,6 @@
     @include('frontend.pages.About.about')
     @include('frontend.pages.Services.services')
     @include('frontend.pages.Details.details')
+    @include('frontend.pages.Projects.projects')
 
 @endsection
